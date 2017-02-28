f08e0b0d1c9994c9f776714d856b80e277024125
Ramon Reyes <reyesjunk@gmail.com>
2017-02-28 16:25:38 -0500

    finished dirll

--- app.js
+++ app.js
@@ -31,3 +31,22 @@ var fr = greeterFr("Sofia");
 
 console.log(eng);
 console.log(fr);
+
+//forEach, filter and map
+function tMovement(array){
+	let filtered = array.filter(function(arr){
+		if(arr[0]>=0 && arr[1]>=0){
+			return arr;
+		}
+
+	});
+	let mapped = filtered.map(function(arr){
+		return arr[0] + arr[1];
+	});
+	mapped.forEach(function(steps){
+		console.log(`The turtle walked ${steps}`);
+	});
+
+}
+
+tMovement([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]);
